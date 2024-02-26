89438d86cba045183bcd461d52c802d463683fc4
zoeng6project <161369393+zoeng6project@users.noreply.github.com>
2024-02-26 14:26:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,32 @@ app.get("/", function (req, res) {
 });
 
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date (req.params.date)
+  if (isInvalidDate(date)){
+    date = new Date (+req.params.date)
+    }
+  if (isInvalidDate(date)){
+    res.json({error: "Invalid Date"})
+    return;
+    }
+
+    res.json ({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+
 });
+
+app.get ("/api" , (req,res) => {
+  res.json ( {
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
 
 
 
